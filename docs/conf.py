d7dd8d1e445df89ac52e2cf35accb98f79c0c331
“JBWeiss-chem” <jan.benedikt.weiss@gmail.com>
2025-09-18 14:06:36 +0200

    Initial commit

--- docs/conf.py
+++ docs/conf.py
@@ -16,12 +16,12 @@ author = 'Jan-Benedikt Weiß'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['../build/html/_static']
+html_theme = 'alabaster'
+html_static_path = ['_static']
